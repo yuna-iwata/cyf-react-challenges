9d83bacf752d1cd09a1c0e4cedf005fbf71194f5
Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
2023-01-08 05:29:30 +0000

    score

--- challenge-country-capitals/my-app/src/components/CapitalsContainer.js
+++ challenge-country-capitals/my-app/src/components/CapitalsContainer.js
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 
 export default function CapitalsContainer({ capitalOptions, randomCountry }) {
   const [message, setMessage] = useState("");
+  const [score, setScore] = useState(0);
 
   const onCapitalClick = (correct) => {
     if (correct) {
       console.log("correct");
       setMessage("correct");
+      setScore(score + 1);
     } else {
       console.log("incorrect");
       setMessage("incorrect");
@@ -29,6 +31,7 @@ export default function CapitalsContainer({ capitalOptions, randomCountry }) {
   });
   return (
     <div>
+      <p>{score}</p>
       {allButtons}
       <p>{message}</p>
     </div>
